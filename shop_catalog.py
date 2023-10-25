60a8ecdffedc5c0dbaa30fa22804837cf408deaa
Cenivell <michutka20085@gmail.com>
2023-10-25 23:49:43 +0300

    Fully re-made the code now are the items are saved as object inside a list. And the list is being printed to the console at the end of the session.

--- shop_catalog.py
+++ shop_catalog.py
@@ -23,73 +23,51 @@
 # Рішення нижче
 ###########################
 
-
 import random
-import os
 from datetime import datetime
 
 
-def check_expire_data(x):
-    expire_date = datetime.strptime(x.product_expdata, "%d/%m/%Y")
+def check_expire_data(product) -> bool:
+    '''Функція для перевірки чи ще термін придатності дійсний'''
+    expire_date = datetime.strptime(product.expdate, "%d/%m/%Y")
     present = datetime.now()
     if expire_date.date() > present.date():
-        x.printname()
         print("Next product:")
+        x = True
     else:
         print("Your product has expired")
+        x = False
+    return x
 
 
-def write_value_to_the_catalog(ptype: str, name: str, price: str, expdata: str, code: str, catalog):
-    '''Функція для того щоб виписувати продукт в файл'''
-    catalog.write(str(f"Type:{ptype}   "))
-    catalog.write(str(f"Name:{name}   "))
-    catalog.write(str(f"Price:{price}   "))
-    catalog.write(str(f"Expire data:{expdata}   "))
-    catalog.write(str(f"Code:{code}    "))
-    catalog.write("\n")
-
-
-def add_new_item():
-    '''Функція для додавання нового предмета/продукта в каталог'''
-    code = random.randrange(10000000, 99999999)
-    new_product = Product(input("What is your product's type? "), input("What is your product's name? "), input("What is your product's price? "), input("What is your product's expire data? (type it like this 15/10/2025) "), code)
-    #Перевіряю чи правильно написана дата та чи срок годності дійсний
-    try:
-        check_expire_data(new_product)
-    except Exception:
-        print("Expire date was redeemed incorrectly")
-
-
-print("Welcome do u wish to use manual directory input? If not then script's directory will be used")
-answer = input("type Y or N ").lower()
-
-if answer == "y":
-    file_directory = input("Please redeem the directory of the file that you wanna use (example D:\menu\drinks) ")
-    file_name = os.path.basename(file_directory)
-    file = f'{file_directory}.txt'
-
-elif answer == "n":
-    file_directory = os.getcwd()
-    file_name = input("Please redeem your file's name ")
-    file = str(f'{file_directory}\{file_name}.txt')
-else:
-    print("You have to print either Y or N")
-    exit(0)
+#Список в якому будуть зберігатися продукти
+products = []
 
 
 #Об'єкт продукт
 class Product:
     def __init__(self, ptype, pname, price, expdate, pcode):
-        self.product_type = ptype
-        self.product_name = pname
-        self.product_price = price
-        self.product_expdata = expdate
-        self.product_code = pcode
-
-    def printname(self):
-        with open(file, 'a') as catalog:
-            write_value_to_the_catalog(self.product_type, self.product_name, self.product_price, self.product_expdata, self.product_code, catalog)
+        self.type = ptype
+        self.name = pname
+        self.price = price
+        self.expdate = expdate
+        self.code = pcode
 
 
 while True:
-    add_new_item()
+    name = input("Enter product name (or 'exit' to finish): ")
+    if name.lower() == 'exit':
+        break
+    product_type = input("Enter product type: ")
+    price = input("Enter product price: ")
+    code = random.randrange(10000000, 99999999)
+    expdate = input("What is your product's expire date? (type it like this 15/10/2025): ")
+    product = Product(name, product_type, price,expdate,code)
+    try:
+        if check_expire_data(product):
+            products.append(product)
+    except Exception:
+        print("Expire date was redeemed incorrectly")
+print("Here is a list of products:")
+for product in products:
+    print(f"Name: {product.name}, Type: {product.type}, Price: {product.price}, Product will expire {product.expdate}, Code: {product.code}")
